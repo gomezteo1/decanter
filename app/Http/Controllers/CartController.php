471a06827551769db47046ce2daae6a9d4dbe710
gomezteo1 <atehortuamateo123@gmail.com>
2021-08-24 19:58:16 -0500

    24-08-2021 19-58

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Products;
+
+class CartController extends Controller
+{
+    
+    public function shop()
+    {   
+        $products = Products::all();
+        return  dd($products);
+        // return view('shop')->withTitle('E-COMMERCE STORE | SHOP')->with(['products' => $products]);
+    }
+
+    public function cart()  {
+        // $cartCollection = Cart::getContent();
+        $cartCollection = \Cart::getContent();
+        dd($cartCollection);
+        return view('cart')->withTitle('E-COMMERCE STORE | CART')->with(['cartCollection' => $cartCollection]);;
+    }
+}
+
+
+
--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Licoreria'),
 
     /*
     |--------------------------------------------------------------------------
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        
 
         /*
          * Package Service Providers...
@@ -228,6 +229,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Cart' => Darryldecode\Cart\Facades\CartFacade::class
+
 
     ],
 
