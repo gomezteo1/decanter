babe7fb856496b7d7c347e776fd285a90b7ffaff
gomezteo1 <atehortuamateo123@gmail.com>
2021-08-26 19:37:19 -0500

    crudCar p1 26-08-2021

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,20 +5,52 @@ use App\Models\Products;
 
 class CartController extends Controller
 {
-    
     public function shop()
     {   
         $products = Products::all();
         // return $products;
         return view('shop')->withTitle('E-COMMERCE STORE | SHOP')->with(['products' => $products]);
     }
-
     public function cart()  {
         // $cartCollection = Cart::getContent();
         $cartCollection = \Cart::getContent();
-        dd($cartCollection);
+        // dd($cartCollection);
         return view('cart')->withTitle('E-COMMERCE STORE | CART')->with(['cartCollection' => $cartCollection]);;
     }
+    public function add(Request $request){
+        \Cart::add(array(
+            'id'=>$request->id,
+            'name'=>$request->name,
+            'price'=>$request->price,
+            'quantity'=>$request->quantity,
+            'attributes'=>array(
+                'image'=>$request->image,
+                'slug'=>$request->slug,
+            )
+        ));
+        return redirect()->route('cart.index')->with('success_msg','Item is added');
+    }
+
+    public function remove(Request $request){
+        \Cart::remove($request->id);
+        return redirect()->route('cart.index')->with('success_msg', 'Item is removed!');
+    }
+
+    public function update(Request $request){
+        \Cart::update($request->id,
+            array(
+                'quantity' => array(
+                    'relative' => false,
+                    'value' => $request->quantity
+                ),
+        ));
+        return redirect()->route('cart.index')->with('success_msg', 'Cart is Updated!');
+    }
+    public function clear(){
+        \Cart::clear();
+        return redirect()->route('cart.index')->with('success_msg', 'Car is empty');
+    }
+
 }
 
 
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,10 @@ Route::post('/add', 'CartController@add')->name('cart.store');
 Route::post('/update', 'CartController@update')->name('cart.update');
 Route::post('/remove', 'CartController@remove')->name('cart.remove');
 Route::post('/clear', 'CartController@clear')->name('cart.clear');
-
+//news
+Route :: post ('/ update', 'CartController@update')->name('cart.update');
+Route :: post ('/ remove', 'CartController@remove')->name('cart.remove');
+Route::post('/clear', 'CartController@clear')->name('cart.clear');
 // Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
